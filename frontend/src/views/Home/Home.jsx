0085adef5b80b7a8651ac4cd608c541b8d4541f3
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-06-04 14:17:34 -0300

    Home frontend

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -3,8 +3,23 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>
-      <p>Welcome to the home page.</p>
+      <h1>Bem vindo ao Carboard, dashboard de carros</h1>
+      <p>Esta aplicação foi feita utilizando:</p>
+      <ul>
+        <li>Mysql</li>
+        <li>NodeJS 20.x</li>
+        <li>Express API</li>
+        <li>React Vite</li>
+        <li>Docker</li>
+      </ul>
+      <p>Funcionalidades:</p>
+      <ul>
+        <li>Listar carros</li>
+        <li>Adicionar carro</li>
+        <li>Editar carro</li>
+        <li>Excluir carro</li>
+      </ul>
+      <p>Feito por Matheus Julidori para a disciplina C216-L1</p>
     </div>
   );
 };
